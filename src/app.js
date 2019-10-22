3cdb90fc58211f972c6be96ff225f526232dae90
jescamilla <corem96@gmail.com>
2019-10-21 19:17:42 -0600

    fix: fixes routes and mongoose loader

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const config = require('./config');
 
 // Router
 const home = require('./routes/home');
-// const mongooseLoader = require('./config/mongooseLoader');
+const heroes = require('./routes/heroes');
+const mongooseLoader = require('./config/mongooseLoader');
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -24,10 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// const mongoDbConn = mongooseLoader.getConnection();
-// mongoDbConn.then(ans => console.log(ans));
+const mongoDbConn = mongooseLoader.getConnection();
+mongoDbConn.then(ans => console.log('connected to mongo...'));
 
 app.use('/', home);
+app.use('/heroes', heroes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)));
